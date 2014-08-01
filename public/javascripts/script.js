7d4108e822a5915061dd3f6c254694b7a30fa843
nehar <nehar@op.pl>
2014-08-01 12:06:54 +0200

    FieldConstructor

--- public/javascripts/script.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-$('.help-inline').each(function(){
-      if($(this).text().length) {
-          $(this).parent().parent().addClass("has-error");
-      }
-  });
-});
-
